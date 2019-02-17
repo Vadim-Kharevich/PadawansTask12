23ac8515c06feb2053c1d077f346de37272c30ab
Vadim-Kharevich <xarevich99@gmail.com>
2019-02-17 16:53:45 +0300

    Update StringExtension.cs

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,7 +6,23 @@ namespace PadawansTask12
     {
         public static bool AllCharactersAreUnique(string source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (source == "")
+            {
+                throw new ArgumentException();
+            }
+            for (int i = 0; i < source.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (source[j] == source[i])
+                        return false;
+                }
+            }
+            return true;
         }
     }
 }
